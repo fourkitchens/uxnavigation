f55ffff20da9b5b2ea3e419c554a9134da7342b4
joetower <joetower@gmail.com>
2017-04-20 09:39:19 -0500

    Adding a primary nested menu pattern

---
+++ components/js/primary-nested.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  	  $('body').addClass('js');
+		  var $menu = $('.primary.menu.nested'),
+		  	  $menulink = $('.menu-link'),
+		  	  $menuTrigger = $('.has-subnav > a');
+		
+		$menulink.click(function(e) {
+			e.preventDefault();
+			$menulink.toggleClass('active');
+			$menu.toggleClass('active');
+		});
+		
+		$menuTrigger.click(function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			$this.toggleClass('active').next('ul').toggleClass('active');
+		});
+		
+});
